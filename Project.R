9f1a2c583b745db0474dec6c4e19cb04a83c47dd
Nafishsy <nafizahmed273273@gmail.com>
2022-12-08 19:16:02 +0600

    K-fold done

--- Project.R
+++ Project.R
@@ -1,11 +1,15 @@
+install.packages('dplyr')
+install.packages('caret')
+install.packages('class')
+library(class)
+library(caret)
+library(dplyr)
+
 data <- read.csv("mxmh_survey_results.csv",header = TRUE)
 str(data)
 
-library(dplyr)
-
 data<-na_if(data,'')
 data<-na.omit(data)
-
 name<-names(data)
 
 
@@ -23,29 +27,57 @@ normalize <- function(x) {
 
 data.normalized <- as.data.frame(lapply(data.subset[,1:7], normalize))
 
-set.seed(123)
-dat.d <- sample(1:nrow(data.normalized),size=nrow(data.normalized)*0.7,replace = FALSE)
-
-train.data <- data.subset[dat.d,]
-test.data <- data.subset[-dat.d,]
-
-train.data_labels <- data.subset[dat.d,8]
-test.data_labels <-data.subset[-dat.d,8]
-
-install.packages('class')
-library(class)
-
-optimal_K =NROW(train.data_labels)
-sqrt(optimal_K)
 
 
-knn.20 <- knn(train=train.data, test=test.data, cl=train.data_labels, k=20)
+set.seed(200)
 
-ACC.20 <- 100 * sum(test.data_labels == knn.20)/NROW(test.data_labels)
+# KNN 1st method
+  dat.d <- sample(1:nrow(data.normalized),size=nrow(data.normalized)*0.7,replace = FALSE)
+  
+  train.data <- data.subset[dat.d,]
+  test.data <- data.subset[-dat.d,]
+  
+  train.data_labels <- data.subset[dat.d,8]
+  test.data_labels <-data.subset[-dat.d,8]
+  
+  optimal_K =NROW(train.data_labels)
+  sqrt(optimal_K)
+  
+  knn.20 <- knn(train=train.data, test=test.data, cl=train.data_labels, k=15)
+  ACC.20 <- 100 * sum(test.data_labels == knn.20)/NROW(test.data_labels)
+  
+  ConfusionMAT <- confusionMatrix(table(knn.20 ,test.data_labels))
+  
+  knn.20 <- factor(knn.20,levels = c(1,2,3),labels = c("Improve","Worsen","No effect"))
+  knn.20
 
-
-install.packages('caret')
-library(caret)
-confusionMatrix(table(knn.20 ,test.data_labels))
-
-
+# KNN K-fold Method
+  
+  num =nrow(data.subset)
+  
+  num =round(num/10)
+  
+  K10FOLD.ACC= 0
+  
+  for (i in 1:num-1) {
+    j=i*10
+    k=i*10-10
+    
+    train.data <- data.subset[k:j,]
+    test.data <- data.subset[-(k:j),]
+    
+    train.data_labels <- data.subset[k:j,8]
+    test.data_labels <-data.subset[-(k:j),8]
+    
+    k.fold.10 <- knn(train=train.data, test=test.data, cl=train.data_labels, k=10)
+    ACC.k.fold <- 100 * sum(test.data_labels == k.fold.10)/NROW(test.data_labels)
+    
+    ConfusionMAT.k.fold <- confusionMatrix(table(k.fold.10 ,test.data_labels))
+    
+    k10.fold <- factor(k.fold.10,levels = c(1,2,3),labels = c("Improve","Worsen","No effect"))
+    k10.fold
+    K10FOLD.ACC= K10FOLD.ACC + ACC.k.fold
+  }
+  print(K10FOLD.ACC/num)
+  
+  
