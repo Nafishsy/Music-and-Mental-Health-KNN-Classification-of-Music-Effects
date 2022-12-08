c1f98623e0564b1a2c5a53e575fd29e2c802b6c1
Nafishsy <nafizahmed273273@gmail.com>
2022-12-08 09:36:25 +0600

    update

--- Project.R
+++ Project.R
@@ -8,24 +8,132 @@ data<-na.omit(data)
 
 name<-names(data)
 
-mainData <- subset(data, select = -c(Timestamp,Age,Primary.streaming.service,Permissions))
+for (i in name)
+{
   
-  # data$Primary.streaming.service <- unclass(factor(data$Primary.streaming.service))
-  # data$While.working <- unclass(factor(data$While.working))
-  # data$Music.effects <- unclass(factor(data$Music.effects))
-  # data$Frequency..Video.game.music. <- unclass(factor(data$Frequency..Video.game.music.))
-  # data$Frequency..R.B.  <- unclass(factor(data$Frequency..R.B. ))
-  # data$Frequency..Pop.  <- unclass(factor(data$Frequency..Pop. ))
-  # data$Frequency..Metal.  <- unclass(factor(data$Frequency..Metal. ))
-  # data$Frequency..Metal.  <- unclass(factor(data$Latin ))
-  # 
-  
-  for (i in name)
+  if(typeof(data[,i])=="character")
   {
-    if(typeof(data[,i])=="character")
-    {
-      data[,i]  <- unclass(factor(data[,i]))
-    }
+    print(i)
+    data[,i]  <- NULL
+  }
+}
+
+data<-na.omit(data)
+
+hist(data$BPM)
+
+for (i in 1:nrow(data))
+{
+  
+  if(data[i,3] < 60 || data[i,3] >115)
+  {
+    print(data[i,3])
+    data[i,3]<-NA
+    
+  }else{
+    
   }
 
-data2<-scale(mainData)
+  
+}
+
+data<-na.omit(data)
+
+
+hist(data$Age)
+
+
+for (i in 1:nrow(data))
+{
+  
+  if(data[i,1] > 50)
+  {
+    print(data[i,1])
+    data[i,1]<-NA
+  }else{
+    
+  }
+  
+  
+}
+
+data<-na.omit(data)
+
+
+hist(data$Hours.per.day)
+
+
+for (i in 1:nrow(data))
+{
+  
+  if(data[i,2] > 14)
+  {
+    print(data[i,1])
+    data[i,2]<-NA
+  }else{
+    
+  }
+  
+}
+
+data<-na.omit(data)
+
+
+hist(data$OCD)
+for (i in 1:nrow(data))
+{
+  
+  if(data[i,7] > 5.5)
+  {
+    print(data[i,7])
+    data[i,7]<-NA
+  }else{
+    
+  }
+  
+}
+data<-na.omit(data)
+
+
+hist(data$Insomnia)
+# for (i in 1:nrow(data))
+# {
+#   
+#   if(data[i,7] > 5.5)
+#   {
+#     print(data[i,7])
+#     data[i,7]<-NA
+#   }else{
+#     
+#   }
+#   
+# }
+data<-na.omit(data)
+
+
+
+
+name=names(data)
+for (i in name)
+{
+  cat(range(data[,i]),'For ',i,'\n')
+  
+}
+
+data2<-scale(data)
+
+library('factoextra')
+fviz_nbclust(data2,kmeans,method= 'wss')
+
+km<- kmeans(data2,centers = 2,nstart = 25)
+km
+
+df_member <-cbind(data2,cluster = km$cluster)
+fviz_cluster(km,data = data2)
+
+arrg<-aggregate(data, by=list(cluster=km$cluster),mean)
+arrg
+
+
+
+
